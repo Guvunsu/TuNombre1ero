6d718f82ae8ab63231af913135cdd701e2ae9f98
TPGGschool <gavryk_fray_rojas@hotmail.com>
2023-10-18 16:09:37 -0600

    TuNombre1ero

--- Assets/Movementcube.cs
+++ Assets/Movementcube.cs
@@ -5,28 +5,26 @@ using UnityEngine;
 
 public class Movementcube : MonoBehaviour
 {
+
+    
    
-    public float roospeed;
-    public Vector3 direction = new Vector3(0, 0, 0);
     public float speed = 5f;
-    public GameObject Gun;
-    public GameObject Bullet;
     public GameObject objectToSpawn;
-    public Transform SpawnPLaceholder;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input .GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-        Instantiate(objectToSpawn, transform.position,Quaternion.identity);
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
 
         }
 
@@ -37,9 +35,9 @@ public class Movementcube : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.S))
         {
-        transform.Translate(Vector3.down.normalized * dt * speed);
-          
+            transform.Translate(Vector3.down.normalized * dt * speed);
+
         }
-       
+
     }
 }
---
+++ Assets/susenemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class susenemy : MonoBehaviour
+{
+    public GameObject Enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        float time = 0;
+        float numero = Random.Range(10, 20);
+        Vector3 RandomPosition = new Vector3(0, numero, 0);
+        if ( time <0)
+        {
+            Instantiate(Enemy, RandomPosition, Quaternion.identity);
+        }
+    }
+}
